e56bdab4db06584bffcc074ecda47ec6ff10ec8c
Kirill <kupuk132@gmail.com>
2021-07-31 18:12:37 +0300

    update cards

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,18 +1,24 @@
 <template>
-  <v-card width="250" class="mx-4">
-    <v-card-title>
-      {{locationData.title}}
-    </v-card-title>
-    <hr>
-    <v-card-text>
-      temp: {{ Math.round(weatherData.main.temp -  273.15)}}
-    </v-card-text>
-    <v-card-actions>
-      <v-btn @click="deleteLocation">
-        delete
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <div>
+    <div v-if="loader"></div>
+    <v-card v-else width="250" class="mx-4">
+      <v-card-title>
+        {{locationData.title}}
+      </v-card-title>
+      <hr>
+      <v-card-text>
+        temp: {{ calcTemp }}
+        feels like: {{ calcFeelsLike }}
+        humidity: {{ this.weatherData.main.humidity }}
+        pressure: {{ this.weatherData.main.pressure }}
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="deleteLocation">
+          delete
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -25,16 +31,26 @@ export default {
   },
   async created() {
     this.weatherData = await getWeatherByCity(this.locationData.title);
+    this.loader = false
     console.log(this.weatherData)
   },
   data: () => ({
     weatherData: null,
+    loader: true
   }),
   methods: {
     deleteLocation() {
       this.$emit('deleteLocation', this.locationData.title)
     },
 
+  },
+  computed: {
+    calcTemp() {
+      return Math.round(this.weatherData.main.temp -  273.15)
+    },
+    calcFeelsLike() {
+      return Math.round(this.weatherData.main.feels_like -  273.15)
+    }
   }
 }
 </script>
