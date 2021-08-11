e9ad83151536bad36b077145b65db000595e5fd4
Kirill <kupuk132@gmail.com>
2021-08-11 19:22:29 +0300

    calcWindDirection

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -2,74 +2,52 @@
   <div>
     <div v-if="loader"></div>
     <v-card v-else width="350" class="mx-4">
-      <v-card-title :class='calcColor'>
+      <v-card-title :class="calcColor">
         {{ locationData.title }}
       </v-card-title>
       <v-card-text>
-        <span
-          class="title"
-        >
-        Temp:  {{ calcTemp }}&#176;
-        </span>
+        <span class="title"> Temp: {{ calcTemp }}&#176; </span>
 
         <v-icon class="float-right" style="font-size: 150px">
-          {{calcIcon}}
+          {{ calcIcon }}
         </v-icon>
 
-        <span
-          class="title d-block"
-        >
+        <span class="title d-block">
           Feels like: {{ calcFeelsLike }}&#176;
         </span>
 
-        <span
-          class="title d-block"
-        >
+        <span class="title d-block">
           <v-icon>
-            mdi-water-percent
-          </v-icon>:
-          {{ this.weatherData.main.humidity }}%
+            mdi-water-percent </v-icon
+          >: {{ this.weatherData.main.humidity }}%
         </span>
 
-        <span
-          class="title d-block"
-        >
+        <span class="title d-block">
           <v-icon>
-            mdi-weight
-          </v-icon>:
-          {{ calcPressure }}-mm
+            mdi-weight </v-icon
+          >: {{ calcPressure }}-mm
         </span>
 
-        <span
-          class="title d-block"
-        >
+        <span class="title d-block">
           <v-icon>
-            mdi-weather-windy
-          </v-icon>:
-          {{ this.weatherData.wind }}
+            mdi-weather-windy </v-icon
+          >:
+           Direction {{calcWindDirection}}, speed: {{this.weatherData.wind.speed}}
         </span>
 
-        <span 
-          class="title d-block"
-          v-if='this.weatherData.rain'
-        >
+        <span class="title d-block" v-if="this.weatherData.rain">
           <v-icon>
-            mdi-water
-          </v-icon>:
-          {{this.weatherData.rain}}
+            mdi-water </v-icon
+          >:
+          {{ this.weatherData.rain }}
         </span>
 
-         <span 
-          class="title d-block"
-          v-if='this.weatherData.snow'
-        >
+        <span class="title d-block" v-if="this.weatherData.snow">
           <v-icon>
-            mdi-snowflake
-          </v-icon>:
-          {{this.weatherData.snow}}
+            mdi-snowflake </v-icon
+          >:
+          {{ this.weatherData.snow }}
         </span>
-
-
       </v-card-text>
       <v-card-actions>
         <v-btn class="mx-auto red" @click="deleteLocation">
@@ -113,36 +91,76 @@ export default {
       return Math.round(this.weatherData.main.pressure / 1.333);
     },
     calcColor() {
-      if(this.weatherData.rain) {
-        return 'grey lighten-1'
+      if (this.weatherData.rain) {
+        return "grey lighten-1";
       }
-      if(this.weatherData.snow) {
-        return 'white'
-      }
-      else {
-        return 'light-blue lighten-3'
+      if (this.weatherData.snow) {
+        return "white";
+      } else {
+        return "light-blue lighten-3";
       }
     },
     calcIcon() {
-      if(this.weatherData.rain) {
-        return 'mdi-weather-pouring'
+      if (this.weatherData.rain) {
+        return "mdi-weather-pouring";
+      } else if (this.weatherData.snow) {
+        return "mdi-weather-snowy";
+      } else if (this.weatherData.clouds.all === 0) {
+        return "mdi-white-balance-sunny";
+      } else if (this.weatherData.clouds.all <= 70) {
+        return "mdi-weather-partly-cloudy";
+      } else if (this.weatherData.clouds.all > 70) {
+        return "mdi-cloud";
+      } else {
+        return null;
       }
-      else if(this.weatherData.snow) {
-        return 'mdi-weather-snowy'
-      }
-      else if (this.weatherData.clouds.all === 0) {
-        return 'mdi-white-balance-sunny'
-      }
-      else if (this.weatherData.clouds.all <= 70) {
-        return 'mdi-weather-partly-cloudy'
-      }
-      else if (this.weatherData.clouds.all > 70) {
-        return 'mdi-cloud'
+    },
+    calcWindDirection() {
+      if (
+        this.weatherData.wind.deg >= 157.5 &&
+        this.weatherData.wind.deg < 202.5
+      ) {
+        return "to South";
+      } else if (
+        this.weatherData.wind.deg >= 202.5 &&
+        this.weatherData.wind.deg < 247.5
+      ) {
+        return "to SouthWest";
+      } else if (
+        this.weatherData.wind.deg >= 247.5 &&
+        this.weatherData.wind.deg < 292.5
+      ) {
+        return "to West";
+      } else if (
+        this.weatherData.wind.deg >= 292.5 &&
+        this.weatherData.wind.deg < 337.5
+      ) {
+        return "to NorthWest";
+      } else if (
+        this.weatherData.wind.deg >= 337.5 ||
+        this.weatherData.wind.deg < 22.5
+      ) {
+        return "to North";
+      } else if (
+        this.weatherData.wind.deg >= 22.5 &&
+        this.weatherData.wind.deg < 67.5
+      ) {
+        return "to NorthEast";
+      } else if (
+        this.weatherData.wind.deg >= 67.5 &&
+        this.weatherData.wind.deg < 112.5
+      ) {
+        return "to East";
+      } else if (
+        this.weatherData.wind.deg >= 112.5 &&
+        this.weatherData.wind.deg < 157.5
+      ) {
+        return "to SouthEast";
       }
       else {
-        return null
+        return 'error in calcs'
       }
-    }
+    },
   },
 };
 </script>
