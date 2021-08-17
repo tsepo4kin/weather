cd1596b158be80c3979c1cd30b932b8b29c3f658
Kirill <kupuk132@gmail.com>
2021-08-17 23:36:53 +0300

    show RU help cities name

--- src/components/InputLocation.vue
+++ src/components/InputLocation.vue
@@ -9,7 +9,12 @@
           clearable
           label="Volzhskiy"
           v-model="cityTitle"
+          @input="autoComplete"
         ></v-text-field>
+        <div
+          v-for='cityName of autoCompleteData'
+          :key='cityName.id'
+        >{{cityName.name}}</div>
         <v-btn class="my-3 d-block mx-auto green accent-2" width="300" @click="addNewLocation">add</v-btn>
       </v-col>
     </v-row>
@@ -17,12 +22,28 @@
 </template>
 
 <script>
+import citiesList from '@/city-list.json'
 export default {
   data: () => ({
     cityTitle: null,
+    autoCompleteData: null
   }),
+  created() {
+    this.citiesListData = citiesList
+  },
   methods: {
-    autoComplete() {},
+    autoComplete(inputValue) {
+      let count = 0;
+      this.autoCompleteData = this.citiesListData.filter(e => {
+        if(e.name.toLowerCase().indexOf(inputValue.toLowerCase()) == 0 && count < 4 ) {
+          count++;
+          return true;
+        } else {
+          return false;
+        }
+      })
+      console.log(this.autoCompleteData)
+    },
     addNewLocation() {
       this.$emit("addNewLocation", this.cityTitle);
       this.cityTitle = null;
